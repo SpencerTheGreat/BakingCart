82400dbd4086c436f5ee90bcf9d6d3cbd7897a81
spencer <cbarton6121@southwestuniversity.edu>
2021-01-22 10:38:18 -0700

    Module 6 finished

---
+++ BakeryShoppingCart/Array.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace BakeryShoppingCart
+{
+    public class Array
+    {
+        public Array()
+        {
+            int[] myArray = new int[5];
+
+            myArray[0] = 1;
+            myArray[1] = 2;
+            myArray[2] = 3;
+            myArray[3] = 4;
+            myArray[4] = 5;
+             
+            }
+        }
+    }
+
+
+
