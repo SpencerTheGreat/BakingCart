72e8cc8dae6dcd7895033d403953ee6bfb69d416
spencer <cbarton6121@southwestuniversity.edu>
2021-01-22 10:18:38 -0700

    finished Module 4

--- BakeryShoppingCart/Program.cs
+++ BakeryShoppingCart/Program.cs
@@ -4,11 +4,25 @@ using BakeryShoppingCart.Models;
 namespace BakeryShoppingCart
 {
     class Program
-    { 
+    {
         static void Main(string[] args)
         {
-            string myVariable = "string";
+            DateTime myFirstDateTimeVariable = new DateTime(2021, 01, 22);
+            DateTime mySecondDateTimeVariable = new DateTime(2030, 05, 10);
 
+            int result = DateTime.Compare(myFirstDateTimeVariable, mySecondDateTimeVariable);
+
+            if (result == 1)
+            {
+                Console.WriteLine("The second date is greater");
+            }
+            else
+            {
+                Console.WriteLine("The first date is greater");
+            }
+                
         }
+        
+    
     }
 }
