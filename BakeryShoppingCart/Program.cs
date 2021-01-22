5fd54dccadfe0fb80e9a8e2f8ef787beb3c0a1de
spencer <cbarton6121@southwestuniversity.edu>
2021-01-22 10:06:13 -0700

    I finished module 2

--- BakeryShoppingCart/Program.cs
+++ BakeryShoppingCart/Program.cs
@@ -7,14 +7,7 @@ namespace BakeryShoppingCart
     { 
         static void Main(string[] args)
         {
-            User newUser = new User();
-            newUser.UserName = "Cory";
-            newUser.Password = "PAssword";
-            newUser.Email = "email@somewhere.com";
-
-            Console.WriteLine("My user is: " + newUser.UserName);
-            Console.WriteLine("And my password is: " + newUser.Password);
-            Console.WriteLine("My email is: " + newUser.Email);
+            string myVariable = "string";
 
         }
     }
